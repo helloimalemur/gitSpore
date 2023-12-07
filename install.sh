e59f85ec2645490edde6fa83327eae1b29e9b3a9
foxx <james.k@konnektive.com>
2023-12-07 18:24:56 -0500

    .

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@
 ## erase install folder and recreate it
 rm -rf /var/lib/gitspore/
 mkdir /var/lib/gitspore/
+mkdir /var/lib/gitspore/config/
 
 ## run bulid
 cargo build --release
 
 ## copy binary and host_list.txt to install dir
 cp target/release/gitspore /var/lib/gitspore/gitspore
-cp -r config/ /var/lib/gitspore/
 if [[ -f /root/.gitspore_settings ]]; then cp /root/.gitspore_settings /var/lib/gitspore/config/Settings.toml; else echo "na"; fi
 cp -r run.sh /var/lib/gitspore/
 
