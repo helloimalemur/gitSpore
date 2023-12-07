d4548246da1c47dbaf674981474bc9b64f7eb4ff
foxx <james.k@konnektive.com>
2023-12-07 18:14:57 -0500

    .

--- install.sh
+++ install.sh
@@ -15,6 +15,7 @@ cargo build --release
 ## copy binary and host_list.txt to install dir
 cp target/release/gitspore /var/lib/gitspore/gitspore
 cp -r config/ /var/lib/gitspore/
+if [[ -f /root/.gitspore_settings ]]; then cp /root/.gitspore_settings /var/lib/gitspore/config/Settings.toml; else echo "na"; fi
 cp -r run.sh /var/lib/gitspore/
 
 
